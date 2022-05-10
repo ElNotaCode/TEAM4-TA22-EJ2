769ae50ec5110593bbcd0d55905a7dcbed2c5d84
Borja Montseny <borja.montseny@gmail.com>
2022-05-10 12:49:36 +0200

    commit inicial rama vista

--- ej2/src/main/java/com/TEAM4_TA22/ej2Main/MainApp.java
+++ ej2/src/main/java/com/TEAM4_TA22/ej2Main/MainApp.java
@@ -1,10 +1,10 @@
-package com.TEAM4_TA22.ej2;
+package com.TEAM4_TA22.ej2Main;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class MainApp 
 {
     public static void main( String[] args )
     {
