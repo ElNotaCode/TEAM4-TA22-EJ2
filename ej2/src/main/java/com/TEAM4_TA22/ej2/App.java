7101113a7cc5911405e8edf21d85ebed6aedd1f1
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-10 10:17:31 +0200

    estructura de proyecto, falta poner las dependencias del JUnit5

---
+++ ej2/src/main/java/com/TEAM4_TA22/ej2/App.java
@@ -0,0 +1,13 @@
+package com.TEAM4_TA22.ej2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ej2/src/test/java/com/TEAM4_TA22/ej2/AppTest.java
@@ -0,0 +1,20 @@
+package com.TEAM4_TA22.ej2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
