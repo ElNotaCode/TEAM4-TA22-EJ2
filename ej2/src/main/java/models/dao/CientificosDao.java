6b020b9d2dea024d18007fbaef75bf03de665a28
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-11 17:19:18 +0200

    Read Cientificos

--- ej2/src/main/java/models/dao/CientificosDao.java
+++ ej2/src/main/java/models/dao/CientificosDao.java
@@ -1,5 +1,6 @@
 package models.dao;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,32 +12,69 @@ public class CientificosDao {
 	//CREATE
 	//createCientifico, le pasamos por parametro un objeto cientifico
 	public void createCientifico(CientificosDto cientifico) {
-		//accedemos a la base de datos
+        //accedemos a la base de datos
+        ConnectionDB conexion = new ConnectionDB();
+
+            try {
+                //creamos statement
+                Statement statement = conexion.crearConexion().createStatement();
+                //creamos la query y las variables las rellenamos con los getters del objeto
+                String Querydb = "USE proyectos;";
+                statement.executeUpdate(Querydb);
+
+                String sql = "INSERT INTO cientificos(DNI,NomApels) VALUES"
+                        + "('"+cientifico.getDni()+"','"+cientifico.getNomApels()+"');";
+                //la ejecutamos
+                statement.executeUpdate(sql);
+                //statement close
+                statement.close();
+                //close conexion
+                conexion.closeConnection();
+                //TODO: informamos de que se ha insertado correctamente
+            } catch (SQLException e) {
+                // TODO: informamos de que NO se ha insertado correctamente
+                //pasamos el error por consola
+                System.out.println(e.getMessage());
+            }
+
+    }
+	
+	//READ
+	public void readCientifico(String dni) {
+		//Abrimos conexión.
 		ConnectionDB conexion = new ConnectionDB();
+		//Objeto "en blanco" que vamos a usar para, en caso que exista, rellenar los datos del cientifico en especifico.
+		CientificosDto cientifico = new CientificosDto();
+		//Bool para ver si existe.
+		boolean existe = false;
 		
-			try {
-				//creamos statement
-				Statement statement = conexion.crearConexion().createStatement();
-				//creamos la query y las variables las rellenamos con los getters del objeto
-				String sql = "INSERT INTO cientificos(DNI,NomApels) VALUES"
-						+ "('"+cientifico.getDni()+"'),('"+cientifico.getNomApels()+"');";
-				//la ejecutamos
-				statement.executeUpdate(sql);
-				//statement close
-				statement.close();
-				//close conexion
-				conexion.closeConnection();
-				//TODO: informamos de que se ha insertado correctamente
-			} catch (SQLException e) {
-				// TODO: informamos de que NO se ha insertado correctamente
-				//pasamos el error por consola
-				System.out.println(e.getMessage());
-			}
+		try {
+			//creamos statement
+            Statement statement = conexion.crearConexion().createStatement();
+            //creamos la query y las variables las rellenamos con los getters del objeto
+            String Querydb = "USE proyectos;";
+            statement.executeUpdate(Querydb);
+			//Vamos a usar el prepared statement.
+			//Es lo mismo que un statement, pero se hace al revés.
+			//Primero sql.
+			String sql = "SELECT * FROM cientificos WHERE dni = '?'";
+			PreparedStatement preparedStatement = conexion.crearConexion().prepareStatement(sql);
+			preparedStatement.setString(1, dni);
+			preparedStatement.execute();
+			//Necesto recibir el resultado de la consulta.
+			String dniSelect = preparedStatement.getResultSet().getString("DNI");
+			//cientifico.setDni(dniSelect);
+			String NomApels = preparedStatement.getResultSet().getString("NomApels");
+			//cientifico.setDni(dniSelect);
+			cientifico = new CientificosDto(dniSelect,NomApels);
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+		
+		
 		
 	}
-	
-	//TODO: READ
-	
 	
 	//UPDATE
 	//también se podría hacer con REPLACE?
@@ -63,14 +101,14 @@ public class CientificosDao {
 	}
 	
 	//DELETE
-	public void deleteCientifico(CientificosDto cientifico) {
+	public void deleteCientifico(String dni) {
 		
 		ConnectionDB conexion = new ConnectionDB();
 		
 		try {
 			Statement statement = conexion.crearConexion().createStatement();
 			String sql = "DELETE FROM cientificos "
-					+ "WHERE DNI='"+cientifico.getDni()+"';";
+					+ "WHERE DNI='"+ dni +"';";
 			statement.execute(sql);
 			statement.close();
 			conexion.closeConnection();
